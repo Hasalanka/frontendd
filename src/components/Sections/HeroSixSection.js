0fbae26984074edfe45dd2a2b7d7efd542dbefce
Anuk87 <anuk08787@gmail.com01~>
2023-05-06 23:22:18 +0530

    create file structure and home

--- src/components/Sections/HeroSixSection.js
+++ src/components/Sections/HeroSixSection.js
@@ -33,7 +33,7 @@ const HeroSixSection = () => {
               <h4 className={classes.header_text}>Paul James</h4>
               <p className={classes.text_location}>Lagos state</p>
               <p className={classes.text_content}>
-                With a Homely Eats meal, you can serve delectable, home-cooked
+                With a Fresco Eats meal, you can serve delectable, home-cooked
                 meals to your family and friends in any location, anytime.
                 anyday!
               </p>
